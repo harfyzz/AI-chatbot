c7e1b86bd07c2f64012e9befbf56155f6e8096c2
Afeez <harfyzz@gmail.com>
2024-06-18 20:12:34 +0100

    Initial Commit

---
+++ AI chatbot/AI_chatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_chatbotApp.swift
+//  AI chatbot
+//
+//  Created by Afeez Yunus on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct AI_chatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI chatbot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI chatbot
+//
+//  Created by Afeez Yunus on 18/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
