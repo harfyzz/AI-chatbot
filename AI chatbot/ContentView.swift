8152093e70150f678e31f0d7e820d2030665d5e7
Afeez <harfyzz@gmail.com>
2024-06-23 18:10:34 +0100

    -added Rive Ai model -added materials -styled further

--- AI chatbot/ContentView.swift
+++ AI chatbot/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import GoogleGenerativeAI
 import SwiftData
+import RiveRuntime
 
 struct ContentView: View {
     
@@ -16,20 +17,21 @@ struct ContentView: View {
     
     
     let geminiModel = GenerativeModel(name: "gemini-1.5-flash", apiKey:APIKey.default )
+    @State var violet = RiveViewModel(fileName: "violet", fit: .contain)
     @State var userMessage = ""
     @State var response = ""
     @State var isLoading = false
     var body: some View {
-        VStack {
-            Text("Chat with Gemini")
-                .font(.title)
-                .padding()
+        ZStack {
             ScrollViewReader{proxy in
                 ScrollView{
+                    VStack{
                     ForEach (messages) { text in
                         ChatBubble(isSentByYou: text.isSentByYou, textInBubble:LocalizedStringKey( text.content), sender: text.sender, timeSent: text.time)
                             .padding(.bottom, 12)
                     }
+                }.padding(.bottom, 70)
+                    .padding(.top, 88)
                     if isLoading {
                         ZStack{
                             HStack{
@@ -49,6 +51,7 @@ struct ContentView: View {
                         }
                     }
                 }
+                .padding(.horizontal)
                 .scrollIndicators(.hidden)
                 .clipShape(RoundedRectangle(cornerRadius: 16))
                 .defaultScrollAnchor(.bottom)
@@ -60,27 +63,45 @@ struct ContentView: View {
                     }
                 }
             }
-            HStack(alignment:.bottom){
-                TextField("Enter message here", text: $userMessage, axis: .vertical)
-                    .lineLimit(4)
-                    .padding(12)
-                    .background(.gray.opacity(0.1))
-                    .clipShape(RoundedRectangle(cornerRadius: 32))
-                Button(action: {
-                    sendMessage()
-                    generateResponse()
-                    
-                    
-                }, label: {
-                    Image(systemName: "arrow.up")
-                        .foregroundStyle(.white)
-                        .padding()
-                        .background(.blue)
-                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                })
-                .disabled(userMessage.isEmpty ? true : false)
-            }.padding(.bottom, 12)
-        }.padding(.horizontal)
+            VStack{
+            HStack{Spacer()
+                VStack(spacing:8){
+                    violet.view()
+                        .frame(height: 44)
+                    Text("Violet")
+                        .font(.headline)
+                        .fontWeight(.medium)
+                        .foregroundStyle(.gray)
+                        .padding(.bottom)
+                }
+                Spacer()
+            }.background(.ultraThinMaterial)
+                Spacer()
+                HStack(alignment:.bottom){
+                    TextField("Talk to Violet...", text: $userMessage, axis: .vertical)
+                        .lineLimit(4)
+                        .padding(12)
+                        .background(.gray.opacity(0.1))
+                        .clipShape(RoundedRectangle(cornerRadius: 32))
+                    Button(action: {
+                        sendMessage()
+                        generateResponse()
+                        
+                        
+                    }, label: {
+                        Image(systemName: "arrow.up")
+                            .foregroundStyle(.white)
+                            .padding()
+                            .background(.blue)
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    })
+                    .disabled(userMessage.isEmpty ? true : false)
+                }.padding(12)
+                    .background(.ultraThinMaterial)
+        }
+        }
+            .preferredColorScheme(.light)
+        
         
     }
     
@@ -113,7 +134,7 @@ struct ContentView: View {
                 geminiMessage.content = response
                 geminiMessage.time = Date()
                 geminiMessage.isSentByYou = false
-                geminiMessage.sender = "Gemini"
+                geminiMessage.sender = "Violet"
                 context.insert(geminiMessage)
                 withAnimation {
                     isLoading = false
